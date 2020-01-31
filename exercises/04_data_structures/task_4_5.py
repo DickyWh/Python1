e336092efc7d90c6fd80ae41d927656c852bebb3
DickyWh <nuke-pro@yandex.ru>
2020-01-31 13:12:01 +0000

    Сделал задание 4.5

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -13,3 +13,61 @@
 
 command1 = 'switchport trunk allowed vlan 1,2,3,5,8'
 command2 = 'switchport trunk allowed vlan 1,3,8,9'
+
+
+In [24]: command1 = 'switchport trunk allowed vlan 1,2,3,5,8'                   
+
+In [25]: command2 = 'switchport trunk allowed vlan 1,3,8,9'                     
+
+In [26]: com1 = command1.split()                                                
+
+In [27]: print(com1)                                                            
+['switchport', 'trunk', 'allowed', 'vlan', '1,2,3,5,8']
+
+In [28]: vlans1 = com1[-1].split(',')                                           
+
+In [30]: print(vlans1)                                                          
+['1', '2', '3', '5', '8']
+
+In [31]: com2 = command2.split()                                                
+
+In [32]: print(com2)                                                            
+['switchport', 'trunk', 'allowed', 'vlan', '1,3,8,9']
+
+In [33]: vlans2 = com2[-1].split(',')                                           
+
+In [34]: print(vlans2)                                                          
+['1', '3', '8', '9']
+
+In [35]: vlans = vlans1 + vlans2                                                
+
+In [36]: print(vlans)                                                           
+['1', '2', '3', '5', '8', '1', '3', '8', '9']
+
+In [38]: vlans.sort()                                                           
+
+In [39]: print(vlans)                                                           
+['1', '1', '2', '3', '3', '5', '8', '8', '9']
+
+In [43]: vlans.pop(1)                                                           
+Out[43]: '1'
+
+In [44]: print(vlans)                                                           
+['1', '2', '3', '3', '5', '8', '8', '9']
+
+In [45]: vlans.pop(2)                                                           
+Out[45]: '3'
+
+In [46]: vlans.pop(4)                                                           
+Out[46]: '8'
+
+In [47]: print(vlans)                                                           
+['1', '2', '3', '5', '8', '9']
+
+In [48]: vlans                                                                  
+Out[48]: ['1', '2', '3', '5', '8', '9']
+
+
+
+
+
