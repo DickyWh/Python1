af0b2b6c863278460df3d861c63a82d7d54c3edb
DickyWh <nuke-pro@yandex.ru>
2020-01-31 08:15:21 +0000

    Сделал задание 4.3

--- exercises/04_data_structures/task_4_3.py
+++ exercises/04_data_structures/task_4_3.py
@@ -10,3 +10,16 @@
 '''
 
 config = 'switchport trunk allowed vlan 1,3,10,20,30,100'
+
+
+In [13]: config = 'switchport trunk allowed vlan 1,3,10,20,30,100'              
+
+In [14]: commands = config.split()                                              
+
+In [15]: print(commands)                                                        
+['switchport', 'trunk', 'allowed', 'vlan', '1,3,10,20,30,100']
+
+In [16]: vlans = commands[-1].split(',')                                        
+
+In [17]: print(vlans)                                                           
+['1', '3', '10', '20', '30', '100']
