87096b187eb8341a1b0586c430b6a2b52ca65b21
DickyWh <nuke-pro@yandex.ru>
2020-02-03 14:36:18 +0000

    Сделал задание 4.7

--- exercises/04_data_structures/task_4_7.py
+++ exercises/04_data_structures/task_4_7.py
@@ -10,3 +10,27 @@
 '''
 
 mac = 'AAAA:BBBB:CCCC'
+
+In [1]: mac = 'AAAA:BBBB:CCCC'                                                                            
+
+In [2]: mac = mac.split(':')                                                                              
+
+In [3]: mac                                                                                               
+Out[3]: ['AAAA', 'BBBB', 'CCCC']
+
+In [4]: print(mac)                                                                                        
+['AAAA', 'BBBB', 'CCCC']
+
+In [5]: len(mac)                                                                                          
+Out[5]: 3
+
+In [7]: '{:b}{:b}{:b}'.format(0xAAAA, 0xBBBB, 0xCCCC)                                                     
+Out[7]: '101010101010101010111011101110111100110011001100'
+
+In [8]: mac = '{:b}{:b}{:b}'.format(0xAAAA, 0xBBBB, 0xCCCC)                                               
+
+In [9]: print(mac)                                                                                        
+101010101010101010111011101110111100110011001100
+
+
+
