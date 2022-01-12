b28b30494d44c227ddd9c573766619917016768b
DickyWh <nuke-pro@yandex.ru>
2022-01-12 07:58:35 -0600

    Сделал 6.2b-2022

--- exercises/06_control_structures/task_6_2b-2022.py
+++ exercises/06_control_structures/task_6_2b-2022.py
@@ -10,46 +10,25 @@
     Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+while True:
+    ip = input('Введите IP адрес в формае х.х.х.х: ')
+    octets = ip.split(".")
+    correct_ip = len(octets) == 4
 
-ip = input('Введите IP-адрес в формате х.х.х.х: ')
+    for i in octets:
+        correct_ip = i.isdigit() and 0 <= int(i) <= 255 and correct_ip
 
-ip_correct = False
+        if correct_ip:
+            break
+        print("Непрвильный IP-адрес")
 
-while not ip_correct:
-    if ip.count('.') != 3:
-        print('Неправильный IP-адрес. необходимо ввести три точки между числами')
-#проверка на наличие цифр в октетах
-    elif ip.strip().split('.')[0].isdigit() is False or ip.strip().split('.')[1].isdigit() is False or ip.strip().split('.')[2].isdigit() is False or ip.strip().split('.')[3].isdigit() is False:
-        print('Неправильный IP-адрес, вводите только числа')
-#проверка на разрешенный диапазон цифр(0-255) в октетах
-    elif int(ip.strip().split('.')[0]) not in list(range(0, 256)) or int(ip.strip().split('.')[1]) not in list(range(0, 256)) or int(ip.strip().split('.')[2]) not in list(range(0, 256)) or int(ip.strip().split('.')[3]) not in list(range(0, 256)):
-        print('Неправильный IP-адрес, разрешен только диапазон 0-255')
-#основная проверка из задания 6.2
-    else:
-        ip = ip.strip().split('.')
-        ip_o1 = int(ip[0])
-        ip_o2 = int(ip[1])
-        ip_o3 = int(ip[2])
-        ip_o4 = int(ip[3])
-        unicast = list(range(1, 224))
-        multicast = list(range(224, 240))
-        broadcast = [255,255,255,255]
-        unassigned = [0,0,0,0]
-        if ip_o1 in unicast:
-            print('Это unicast IP-адрес')
-        elif ip_o1 in multicast:
-            print('Это multicast IP-адрес')
-        elif ip_o1 in broadcast and ip_o2 in broadcast and ip_o3 in broadcast and ip_o4 in broadcast:
-            print('Это broadcast IP-адрес')
-        elif ip_o1 in unassigned and ip_o2 in unassigned and ip_o3 in unassigned and ip_o4 in unassigned:
-            print('Это неназначенный IP-адрес')
-        else:
-            print('Это unused (неиспользуемый) IP-адрес')
-        ip_correct = True
-        continue
-    ip = input('Введите IP-адрес ещё раз: ')
-
-
-
-
-
+if 1 <= int(octets[0]) <= 223:
+    print("unicast адрес")
+elif 224 <= int(octets[0]) <= 239:
+    print('multicast адрес')
+elif ip == '0.0.0.0':
+    print('unassigned адрес')
+elif ip == '255.255.255.255':
+    print('local broadcast адрес')
+else:
+    print('неправильный адрес')
